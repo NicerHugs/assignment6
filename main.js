f1033c2a7cffd18857692e798fb424074d7895b7
Jess Scheuring <jscheuring@gmail.com>
2014-09-03 15:00:25 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("update-button").onclick = function(){
+  var thimblesCost = Number(document.getElementById('thimbles').value) * 1;
+  var swordsCost = Number(document.getElementById('swords').value) * 37;
+  var totalCost = swordsCost + thimblesCost;
+  document.getElementById("the-total").textContent = totalCost + " monies";
+}
